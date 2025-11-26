5e92656d3da20e3c1f28950dbde14d11b76195f1
Izaura Honorato <izahonorato@gmail.com>
2025-11-26 16:17:17 -0300

    commit inicial do projeto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainButton } from "./components/main-button/main-button";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainButton],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/main-button/main-button.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-main-button',
+  imports: [],
+  templateUrl: './main-button.html',
+  styleUrl: './main-button.css',
+})
+export class MainButton {
+  @Input() descBotao: string = '';
+
+}
